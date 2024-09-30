40d863ea88ab3f77cf3ed7e106469b3e4a6d0dfe
rohit1246 <rooohit456@gmail.com>
2024-09-30 21:35:17 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,33 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import { useState } from 'react';
-import RefrshHandler from './RefrshHandler';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import { useState } from "react";
+import RefrshHandler from "./RefrshHandler";
+import Logo from "./pages/Logo";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const PrivateRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to="/login" />
-  }
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
 
   return (
-    <div className="App">
-      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
-      </Routes>
+    <div >
+
+      <Logo />
+
+      <div className="App">
+        <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="main-title">
+      FinSmart
+    </div>
+  )
+}
+
+export default Logo
